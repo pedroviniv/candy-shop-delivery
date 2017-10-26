c223132af5e60019ffcd0dc1f7acf654bae9d056
Pedro Arthur F. de Vasconcelos <pfernandesvasconcelos@gmail.com>
2017-10-26 04:13:40 -0200

    migrated to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171026023032) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "orders", force: :cascade do |t|
     t.string "title"
     t.string "description"
